f0945d2fc3e170606600ddf75538b2501230407e
Ryan Day <jrday@users.noreply.github.com>
2018-08-21 17:34:47 -0700

    Update Catalyst3Controller.java

--- src/main/java/com/atomist/Catalyst3Controller.java
+++ src/main/java/com/atomist/Catalyst3Controller.java
@@ -1,5 +1,7 @@
 package com.atomist;
 
+/* comment */
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
